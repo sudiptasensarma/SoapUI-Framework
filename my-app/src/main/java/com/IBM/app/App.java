866333ba03555f1d20996f31d49b31f4fae3c885
Sudipta Sen Sarma <sudisarm@in.ibm.com>
2021-02-24 14:04:41 +0530

    commit soapui

---
+++ my-app/src/main/java/com/IBM/app/App.java
@@ -0,0 +1,13 @@
+package com.IBM.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
